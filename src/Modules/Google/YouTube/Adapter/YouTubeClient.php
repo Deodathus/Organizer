160d138ff58f6b935b3a433014a95da31fef4b00
Bohdan Mykhalevych <bslexmail@gmail.com>
2022-06-30 14:29:25 +0200

    Unused code deleted.

--- src/Modules/Google/YouTube/Adapter/YouTubeClient.php
+++ src/Modules/Google/YouTube/Adapter/YouTubeClient.php
@@ -7,7 +7,6 @@ use App\Modules\Google\YouTube\Adapter\Client as ClientWrapper;
 use App\Modules\Google\YouTube\Entity\ClientAccessToken;
 use App\Modules\Google\YouTube\Entity\ClientConfiguration;
 use Google\Client;
-use Google\Service\YouTube;
 use Google_Service_YouTube;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
@@ -18,77 +17,6 @@ final class YouTubeClient implements YouTubeClientInterface
     public function __construct(
       private readonly UrlGeneratorInterface $urlGenerator
     ) {}
-
-    public function getChannelWithId(string $id): YouTube\ChannelListResponse
-    {
-        $client = new Client(
-            [
-//                'application_name' => $googleClientConfig->getApplicationName(),
-//                'scope' => $googleClientConfig->getScope(),
-//                'client_id' => $googleClientConfig->getClientId(),
-//                'client_secret' => $googleClientConfig->getClientSecret(),
-                'application_name' => 'Organizer',
-                'scope' => 'https://www.googleapis.com/auth/youtube.readonly',
-                'developer_key' => 'AIzaSyAT14G9nLKx4puf7TO9H6YnFd9aNemlr4Y',
-            ]
-        );
-
-        $service = new YouTube($client);
-
-        $queryParams = [
-            'id' => $id,
-        ];
-
-        return $service->channels->listChannels('contentDetails,statistics', $queryParams);
-    }
-
-    public function uploadVideo()
-    {
-        $client = new Client(
-            [
-//                'scope' => $googleClientConfig->getScope(),
-//                'client_id' => $googleClientConfig->getClientId(),
-//                'client_secret' => $googleClientConfig->getClientSecret(),
-                'client_id' => '734671381868-gvl4sthsp3b33vrgi3ejg132v4bcl9rk.apps.googleusercontent.com',
-                'client_secret' => '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',
-                'scope' => 'https://www.googleapis.com/auth/youtube.upload',
-            ]
-        );
-
-        $client->addScope(Google_Service_YouTube::YOUTUBE_UPLOAD);
-        $client->setRedirectUri('http://' . $_SERVER['HTTP_HOST'] . '/api/google/youtube/auth');
-        $client->setAccessType('offline');
-
-        $accessToken = $client->fetchAccessTokenWithAuthCode('4/0AX4XfWjWEaHuqms_VxwDlU0OT8HCyK0vG3VvKa-Q-3hWSIpHD3_w9DKDxJl_IHWbaolMIw');
-
-        dd($accessToken);
-
-        try {
-            $client->setAccessToken($accessToken);
-        } catch (\InvalidArgumentException $exception) {
-            return $client->createAuthUrl();
-        }
-
-        $service = new YouTube($client);
-
-        $video = new YouTube\Video();
-        $videoSnippet = new YouTube\VideoSnippet();
-        $videoSnippet->setTitle('Test video title');
-        $videoSnippet->setDescription('Test video description');
-        $videoSnippet->setTags(['reddit', 'redditvideos']);
-
-        $video->setSnippet($videoSnippet);
-
-        return $service->videos->insert(
-            'snippet',
-            $video,
-            [
-                'data' => file_get_contents(__DIR__ . '/what activities have gotten ha....mp4'),
-                'mimeType' => 'application/octet-stream',
-                'uploadType' => 'multipart',
-            ]
-        );
-    }
 
     public function getUrlToAccessTokenGeneration(ClientConfiguration $clientConfiguration): string
     {
