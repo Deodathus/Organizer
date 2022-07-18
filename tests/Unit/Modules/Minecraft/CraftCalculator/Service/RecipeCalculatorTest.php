b4d019ceb5290099292023d3b68b21df12342ac0
Bohdan Mykhalevych <bslexmail@gmail.com>
2022-07-18 13:05:12 +0200

    Recipe calculator test refactoring.

--- tests/Unit/Modules/Minecraft/CraftCalculator/Service/RecipeCalculatorTest.php
+++ tests/Unit/Modules/Minecraft/CraftCalculator/Service/RecipeCalculatorTest.php
@@ -8,63 +8,127 @@ use App\Modules\Minecraft\CraftCalculator\DTO\ResultDTO;
 use App\Modules\Minecraft\CraftCalculator\Service\Calculator\RecipeCalculator;
 use App\Modules\Minecraft\Item\Adapter\Calculator\Model\Calculable;
 use App\Modules\Minecraft\Item\Entity\Ingredient;
+use App\Modules\Minecraft\Item\Entity\Item;
 use App\Modules\Minecraft\Item\Entity\Recipe;
 use App\Modules\Minecraft\Item\Entity\RecipeResult;
-use App\Tests\Util\Minecraft\Recipe\TestRecipeFactory;
+use Doctrine\Common\Collections\ArrayCollection;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 
 final class RecipeCalculatorTest extends TestCase
 {
-    private const RECIPE_AMOUNT = TestRecipeFactory::RECIPE_AMOUNT;
+    private const RECIPE_AMOUNT = 5;
+
+    private const RECIPE_NAME = 'Iron pickaxe recipe';
 
     private RecipeCalculator $recipeCalculator;
 
-    private TestRecipeFactory $recipeFactory;
+    private readonly Ingredient|MockObject $ironIngotAsIngredient;
 
-    private Recipe|MockObject $recipe;
+    private readonly Ingredient|MockObject $stickAsIngredient;
 
-    private Ingredient $ingredient;
-
-    private RecipeResult $recipeResult;
+    private readonly RecipeResult|MockObject $ironPickaxeAsRecipeResult;
 
     public function setUp(): void
     {
         $this->recipeCalculator = new RecipeCalculator();
-
-        $this->recipeFactory = new TestRecipeFactory();
     }
 
     public function testCalculateSingleItemRecipe(): void
     {
-        $this->setUpRecipe();
+        $recipe = $this->createMock(Recipe::class);
+        $recipe
+            ->method('getName')
+            ->willReturn(self::RECIPE_NAME);
+        $recipe
+            ->method('getIngredients')
+            ->willReturn($this->buildIngredients());
+        $recipe
+            ->method('getResults')
+            ->willReturn($this->buildRecipeResult());
 
         $calculatorResult = $this->recipeCalculator->calculate(
-            new Calculable($this->recipe),
+            new Calculable($recipe),
             self::RECIPE_AMOUNT
         );
 
-        /** @var IngredientDTO $calculatorResultIngredient */
-        $calculatorResultIngredient = $calculatorResult->getIngredients()->first();
+        /** @var IngredientDTO $ironIngotAmountNeeded */
+        $ironIngotAmountNeeded = $calculatorResult->getIngredients()->first();
+        /** @var IngredientDTO $stickAmountNeeded */
+        $stickAmountNeeded = $calculatorResult->getIngredients()->last();
 
-        /** @var ResultDTO $calculatorResultRecipeResult */
-        $calculatorResultRecipeResult = $calculatorResult->getResults()->first();
+        /** @var ResultDTO $ironPickaxeCreated */
+        $ironPickaxeCreated = $calculatorResult->getResults()->first();
 
         $this->assertSame(
-            $this->ingredient->getAmount() * self::RECIPE_AMOUNT,
-            $calculatorResultIngredient->getAmount()
+            $this->ironIngotAsIngredient->getAmount() * self::RECIPE_AMOUNT,
+            $ironIngotAmountNeeded->getAmount()
+        );
+        $this->assertSame(
+            $this->stickAsIngredient->getAmount() * self::RECIPE_AMOUNT,
+            $stickAmountNeeded->getAmount()
         );
 
         $this->assertSame(
-            $this->recipeResult->getAmount() * self::RECIPE_AMOUNT,
-            $calculatorResultRecipeResult->getAmount()
+            $this->ironPickaxeAsRecipeResult->getAmount() * self::RECIPE_AMOUNT,
+            $ironPickaxeCreated->getAmount()
         );
     }
 
-    private function setUpRecipe(): void
+    private function buildIngredients(): ArrayCollection
     {
-        $this->recipe = $this->recipeFactory->build();
-        $this->ingredient = $this->recipeFactory->getIngredient();
-        $this->recipeResult = $this->recipeFactory->getRecipeResult();
+        $ironIngot = $this->createMock(Item::class);
+        $ironIngot
+            ->method('getId')
+            ->willReturn(1);
+        $ironIngot
+            ->method('getName')
+            ->willReturn('Iron ingot');
+        $stick = $this->createMock(Item::class);
+        $stick
+            ->method('getId')
+            ->willReturn(2);
+        $stick
+            ->method('getName')
+            ->willReturn('Stick');
+
+        $this->ironIngotAsIngredient = $this->createMock(Ingredient::class);
+        $this->ironIngotAsIngredient
+            ->method('getId')
+            ->willReturn(1);
+        $this->ironIngotAsIngredient
+            ->method('getAmount')
+            ->willReturn(3);
+
+        $this->stickAsIngredient = $this->createMock(Ingredient::class);
+        $this->stickAsIngredient
+            ->method('getId')
+            ->willReturn(2);
+        $this->stickAsIngredient
+            ->method('getAmount')
+            ->willReturn(2);
+
+        return new ArrayCollection([$this->ironIngotAsIngredient, $this->stickAsIngredient]);
+    }
+
+    private function buildRecipeResult(): ArrayCollection
+    {
+        $ironPickaxe = $this->createMock(Item::class);
+        $ironPickaxe
+            ->method('getId')
+            ->willReturn(3);
+        $ironPickaxe
+            ->method('getName')
+            ->willReturn('Iron Pickaxe');
+
+        $this->ironPickaxeAsRecipeResult = $this->createMock(RecipeResult::class);
+        $this->ironPickaxeAsRecipeResult
+            ->method('getId')
+            ->willReturn(1);
+        $this->ironPickaxeAsRecipeResult
+            ->method('getAmount')
+            ->willReturn(1);
+
+        return new ArrayCollection([$this->ironPickaxeAsRecipeResult]);
     }
 }
