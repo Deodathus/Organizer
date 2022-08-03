5f7728f82bd1617932fb030751641d574e481db8
Bohdan Mykhalevych <bslexmail@gmail.com>
2022-08-03 23:00:41 +0200

    Tree recipe calculator rounding.

--- src/Modules/Minecraft/CraftCalculator/Service/Calculator/TreeRecipeCalculator.php
+++ src/Modules/Minecraft/CraftCalculator/Service/Calculator/TreeRecipeCalculator.php
@@ -107,7 +107,10 @@ final class TreeRecipeCalculator implements TreeRecipeCalculatorInterface
                     throw new RuntimeException('Division by zero!');
                 }
 
-                $amountForResultRecipeIngredient = ($amount * $resultRecipeIngredient->getAmount()) / $resultAmount;
+                $craftMultiplier = ceil($amount / $resultAmount);
+
+                $amountForResultRecipeIngredient = $craftMultiplier * $resultRecipeIngredient->getAmount();
+
                 $alreadyCalculatedIngredients[] = $resultRecipeIngredient->getId();
                 $treeDepth++;
 
