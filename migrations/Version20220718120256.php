23f5955927978ef8a6bbdb6dbf0b3849f8d407e8
Bohdan Mykhalevych <bslexmail@gmail.com>
2022-08-03 18:14:05 +0200

    Makefile fix. Migrations code style changed.

--- migrations/Version20220718120256.php
+++ migrations/Version20220718120256.php
@@ -20,9 +20,25 @@ final class Version20220718120256 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE ingredient_item (ingredient_id INT NOT NULL, item_id INT NOT NULL, INDEX IDX_414A9588933FE08C (ingredient_id), INDEX IDX_414A9588126F525E (item_id), PRIMARY KEY(ingredient_id, item_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE ingredient_item ADD CONSTRAINT FK_414A9588933FE08C FOREIGN KEY (ingredient_id) REFERENCES ingredient (id) ON DELETE CASCADE');
-        $this->addSql('ALTER TABLE ingredient_item ADD CONSTRAINT FK_414A9588126F525E FOREIGN KEY (item_id) REFERENCES items (id) ON DELETE CASCADE');
+        $this->addSql('
+            CREATE TABLE ingredient_item (
+                ingredient_id INT NOT NULL, 
+                item_id INT NOT NULL, 
+                INDEX IDX_414A9588933FE08C (ingredient_id), 
+                INDEX IDX_414A9588126F525E (item_id), 
+                PRIMARY KEY(ingredient_id, item_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
+        ');
+
+        $this->addSql('
+            ALTER TABLE ingredient_item 
+                ADD CONSTRAINT FK_414A9588933FE08C FOREIGN KEY (ingredient_id) REFERENCES ingredient (id) ON DELETE CASCADE
+        ');
+
+        $this->addSql('
+            ALTER TABLE ingredient_item 
+                ADD CONSTRAINT FK_414A9588126F525E FOREIGN KEY (item_id) REFERENCES items (id) ON DELETE CASCADE
+        ');
+
         $this->addSql('ALTER TABLE ingredient DROP FOREIGN KEY FK_6BAF7870126F525E');
         $this->addSql('DROP INDEX IDX_6BAF7870126F525E ON ingredient');
         $this->addSql('ALTER TABLE ingredient DROP item_id');
